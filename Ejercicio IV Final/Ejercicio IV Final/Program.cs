ee99d87c07b06188a39cedc92ed83b1298368e0d
cemarv25 <49078986+cemarv25@users.noreply.github.com>
2019-05-16 12:27:58 -0600

    Ejercicio IV

---
+++ Ejercicio IV Final/Ejercicio IV Final/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_IV_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class SelectionSort
+        {
+            public static void selectionSort<T>(T[] array)
+                where T: IComparable<T>
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    T min = array[i];
+                    int minId = i;
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j].CompareTo(min) < 0)
+                        {
+                            min = array[j];
+                            minId = j;
+                        }
+                    }
+                    // swapping
+                    T temp = array[i];
+                    array[i] = min;
+                    array[minId] = temp;
+                }
+            }
+        }
+    }
+}
